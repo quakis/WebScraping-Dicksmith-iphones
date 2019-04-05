acd0c1ffab8e5d40f6693c9baac7fa63cdc59b38
quakis <41400781+quakis@users.noreply.github.com>
2019-04-05 18:59:43 +1300

    Add files via upload

---
+++ WebScrape.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen as Req
+from bs4 import BeautifulSoup as soup
+
+
+f = open('data.csv', 'w')
+
+url = 'https://www.dicksmith.co.nz/dn/shop/phones/iphone/?page=1'
+
+Client = Req(url)
+page_html = Client.read()
+Client.close()
+
+page_soup = soup(page_html, 'html.parser')
+
+containers = page_soup.find_all('div', class_='_1umis')
+#container = containers[0]
+
+f.write('Phone, Rating, Price \n')
+
+for container in containers:
+    Phone = container.find('a', itemprop='url').text
+    if container.find('meta', itemprop='ratingValue') == None:
+        Rating = 'no rating'
+    else:
+        Rating = container.find('meta', itemprop='ratingValue')['content']
+    Price = container.find('span', itemprop='price')['content']
+    f.write(Phone.replace(',',' |') + ', '+ Rating+', '+ Price + "\n")
+    print(Phone.replace(',',' |') + ', '+ Rating+','+ Price.replace(',','') + "\n")
+
+f.close()
